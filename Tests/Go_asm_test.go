d45d887f547d201dd0c28d78ff1ccf07c84e2956
Senko Minin <swilkfo@gmail.com>
2025-08-30 17:45:21 +0300

    ADD ReadMe

--- Tests/Go_asm_test.go
+++ Tests/Go_asm_test.go
@@ -1,7 +1,11 @@
 package Tests
 
 import (
+	"fmt"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
 	"math/rand"
+	"testing"
 	"unicode"
 )
 
@@ -29,7 +33,7 @@ func GenerateSlice(lenght int) []int32 {
 }
 
 func GenerateSentence(wordCount int) []rune {
-	buff := make([]byte, 0, wordCount*wordLength+wordCount-1)
+	buff := make([]byte, 0)
 	for i := 0; i < wordCount; i++ {
 		for k := 0; k < wordLength; k++ {
 			buff = append(buff, dictionary[rand.Intn(len(dictionary))])
@@ -39,19 +43,18 @@ func GenerateSentence(wordCount int) []rune {
 	return []rune(string(buff[:len(buff)-1]))
 }
 
-//
-//func TestSumSlice(t *testing.T) {
-//	for i := 0; i < attempt; i++ {
-//		length := 100
-//		input := GenerateSlice(length)
-//		actual := lo.Sum[int32](input)
-//		assert.Equal(t, SumSlice(input), actual, fmt.Sprintf("attempt %d", i))
-//	}
-//}
-//
-//func TestCountWord(t *testing.T) {
-//	for i := 0; i < attempt; i++ {
-//		wordNumber := int32(rand.Intn(100))
-//		assert.Equal(t, WordCount(GenerateSentence(int(wordNumber))), wordNumber)
-//	}
-//}
+func TestSumSlice(t *testing.T) {
+	for i := 0; i < attempt; i++ {
+		length := 100
+		input := GenerateSlice(length)
+		actual := lo.Sum[int32](input)
+		assert.Equal(t, SumSlice(input), actual, fmt.Sprintf("attempt %d", i))
+	}
+}
+
+func TestCountWord(t *testing.T) {
+	for i := 0; i < attempt; i++ {
+		wordNumber := int32(rand.Intn(100))
+		assert.Equal(t, WordCount(GenerateSentence(int(wordNumber))), wordNumber)
+	}
+}
